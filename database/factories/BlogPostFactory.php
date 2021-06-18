0fd9aef100862075646269674981e5845fe48bea
muhammadali8767 <muhammadali8767@gmail.com>
2021-06-18 07:23:34 +0500

    first commit

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BlogPost;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogPostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BlogPost::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence(rand(3, 8), true);
+        return [
+            'category_id' => rand(1, 11),
+            'user_id' => User::factory(),
+            'title' => $title,
+
+        ];
+    }
+}
