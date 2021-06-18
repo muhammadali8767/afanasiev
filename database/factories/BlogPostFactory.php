15792d15abebb5e47ac8dfd73d0538f2824c40a1
muhammadali8767 <muhammadali8767@gmail.com>
2021-06-18 07:36:14 +0500

    commit 2

--- database/factories/BlogPostFactory.php
+++ database/factories/BlogPostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Str;
 use App\Models\BlogPost;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -23,11 +24,19 @@ class BlogPostFactory extends Factory
     public function definition()
     {
         $title = $this->faker->sentence(rand(3, 8), true);
+        $txt = $this->realText(rand(1000, 4000));
+        $is_published = rand(1, 5) > 1;
+
         return [
             'category_id' => rand(1, 11),
             'user_id' => User::factory(),
             'title' => $title,
-
+            'slug' => Str::slug($title),
+            'sxcerpt' => $this->text(rand(40, 100)),
+            'content_raw' => $txt,
+            'content_html' => $txt,
+            'is_published' => $is_published,
+            'published_at' => $is_published ? $this->faker->dateTimeBetween('-2 months', '-1 days') : null,
         ];
     }
 }
