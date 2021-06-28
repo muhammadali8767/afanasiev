c54f413d5f66ac4de77338c19b51282cf0975e61
muhammadali8767 <muhammadali8767@gmail.com>
2021-06-28 18:10:43 +0500

    admin layout changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,20 +12,17 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="{{ route('blog.admin.posts.index') }}">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item {{ (request()->segment(3) == 'categories') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('blog.admin.categories.index') }}">Categories <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('blog.admin.categories.index') }}">Categories</a>
-          </li>
-          <li class="nav-item">
+          <li class="nav-item {{ (request()->segment(3) == 'posts') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('blog.admin.posts.index') }}">Posts</a>
           </li>
 
